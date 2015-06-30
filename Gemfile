3a301d74a066d8c208c9ff89f32dc791ad152534
Aditya Varma <aditya.varma@zoomcar.com>
2015-06-30 11:47:43 +0530

    Intialize repository

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.0'
+gem 'sass-rails', '4.0.0'
+gem 'uglifier', '1.3.0'
+gem 'coffee-rails', '4.0.0'
+gem 'jquery-rails', '3.1.3'
+gem 'turbolinks',    '2.3.0'
+gem 'jbuilder', '1.2'
+gem 'sdoc',                 '0.4.0', group: :doc
+
+
+group :development, :test do
+  gem 'sqlite3',     '1.3.9'
+  gem 'byebug',      '3.4.0'
+  gem 'web-console', '2.0.0.beta3'
+  gem 'spring',      '1.1.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelloApp::Application.config.secret_key_base = '7088c72ea87d07a66651ef53bed523f2f63f8e76ad38659a4cc8bdac68802ca7f8b4762343ea967011b82ed79616fb4b6e8a60bc64f8b09bb45af0f693072c80'
